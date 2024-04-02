c4f7bba0ac143639d55fa5f15948405d3fda9b0f
Rene B. Pinto <renebentes@yahoo.com.br>
2024-04-02 10:01:00 -0300

    fix: Restringe execução de scripts PowerShell ao Windows

--- .chezmoiscripts/windows/run_apply-settings-windows-terminal.ps1
+++
@@ -1,4 +0,0 @@
-$SourceFile = "$env:USERPROFILE\.config\windows-terminal\settings.json"
-$DestinationFile = "$env:LOCALAPPDATA\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-
-Copy-Item $SourceFile $DestinationFile
