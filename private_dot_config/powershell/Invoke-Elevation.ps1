27e5f56e28a6038d90f98b86d8df35b46836e2ab
Rene Bentes Pinto <renebentes@yahoo.com.br>
2024-11-17 14:40:19 -0300

    feat: remove debug de comando

--- private_dot_config/powershell/Invoke-Elevation.ps1
+++ private_dot_config/powershell/Invoke-Elevation.ps1
@@ -1,11 +1,8 @@
 function Invoke-Elevation {
-    Write-Output $MyInvocation
-    Write-Output $MyInvocation.MyCommand
     if ($args.Length -eq 1) {
-        Start-Process $args[0] -Verb RunAs
+        Start-Process $args[0] -Verb "runAs"
     }
-
     if ($args.Length -gt 1) {
-        Start-Process $args[0] -ArgumentList $args[1..($args.Length - 1)] -Verb RunAs
+        Start-Process $args[0] -ArgumentList $args[1..$args.Length] -Verb "runAs"
     }
 }
