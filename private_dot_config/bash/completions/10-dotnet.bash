918bace0d3f673be653b46ff8a4c1518a1392f2f
Rene B. Pinto <renebentes@yahoo.com.br>
2025-10-28 13:23:51 -0300

    feat(dotnet): atualize a forma de habilitar o tab completion
    Atualiza conforme recomendado em https://learn.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete

--- private_dot_config/bash/completions/10-dotnet.bash
+++ private_dot_config/bash/completions/10-dotnet.bash
@@ -1,10 +1,11 @@
 # bash parameter completion for the dotnet CLI
 if hash dotnet 2>/dev/null; then
     _dotnet_bash_complete() {
-        local word=${COMP_WORDS[COMP_CWORD]}
-        local completions=("$(dotnet complete --position "${COMP_POINT}" "${COMP_LINE}")")
+        local word=${COMP_WORDS[COMP_CWORD]} IFS=$'\n'
+        local candidates
 
-        COMPREPLY=($(compgen -W "$completions" -- "$word"))
+        read -d '' -ra candidates < <(dotnet complete --position "${COMP_POINT}" "${COMP_LINE}" 2>/dev/null)
+        read -d '' -ra COMPREPLY < <(compgen -W "${candidates[*]:-}" -- "$word")
     }
     complete -f -F _dotnet_bash_complete dotnet
 fi
